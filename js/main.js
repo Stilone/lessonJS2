772832e3564663266b38b6f4ddea2b9d93b315c2
Stilone <54247765+Stilone@users.noreply.github.com>
2021-01-18 00:45:37 +0300

    lesson 2
    complited

--- js/main.js
+++ js/main.js
@@ -1,33 +1,96 @@
 
+class Product {
+    constructor(product, img = 'https://placehold.it/100x50') {
+        let { title, price = 0, id } = product;
+        this.title = title;
+        this.img = img;
+        this.price = price;
+        this.id = id;
+    }
 
-const products = [
-    { id: 1, title:'IPhone 12Pro', price: '100 000р.' },
-    { id: 2, title:'IPhone 12', price: '90 000р.' },
-    { id: 3, title:'IPhone SE', price: '70 000р.'},
-    { id: 4, title:'IPhone 11', price: '65 000р.'},
-    { id: 5, title:'IPhone XR', price: '60 000р.'},
-    { id: 6, title:'IPad Pro', price: '80 000р.'},
-    { id: 7, title:'IPad Air', price: '70 000р.'},
-    { id: 8, title:'iPad mini', price: '60 000р.'},
-    { id: 9, title:'iPad', price: '75 000р.'},
-    { id: 10, title:'MacBook Air', price: '100 000р.'},
-    { id: 11, title:'MacBook Pro 13', price: '200 000р.'},
-    { id: 12, title:'MacBook Pro 16', price: '250 000р.'},
-];
+    render() {
+        return `<div class="product-item">
+                  <img src="${this.img}" alt="${this.title}">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price}</p>
+                      <button class="buy-btn">Купить</button>
+                  </div>
+              </div>`
+    }
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <img class="img-product "src="img/eye.jpg" alt="myImage" width=120 height=80>
-                <h3 class="title-text">${title}</h3>
-                <p class="title-cost">${price} </p>
-                <button class="search-result" type="get">Добавить</button>
-            </div>`
-};
+}
 
-const render = productsList => {
-    const productsElements = productsList.map(item => renderProduct(item.title, item.price)).join('');
-    document.querySelector('.products').innerHTML = productsElements;
+class ProductsList {
+    constructor(container = '.products') {
+        this.data = [];
+        this.products = [];
+        this.container = document.querySelector(container);
+        this._fetchData();
+        this._render();
+        this.summ()
+    }
+
+  
+    _fetchData() {
+        this.data = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Keyboard', price: 200 },
+            { id: 3, title: 'Mouse', price: 100 },
+            { id: 4, title: 'Gamepad' },
+        ];
+    }
+
+
+       
+
+    _render() {
+        for (let dataEl of this.data) {
+            const product = new Product(dataEl);
+            this.products.push(product);
+            this.container.insertAdjacentHTML('beforeend', product.render())
+        }
+    }
+//задание 2.
+    summ() {
+        let total = 0
+        const collectSumm = (product) => {
+            total += product.price;
+            return total;
+        }
     
-};
+        this.products.forEach(collectSumm)
+        return console.log(total);
+    }
 
-render(products);
+}
+
+
+
+const list = new ProductsList();
+
+//задание 1.
+//Добавьте пустые классы для корзины товаров и элемента корзины товаров. 
+//Продумайте, какие методы понадобятся для работы с этими сущностями.
+
+    /*class Cart {
+    constructor() {
+        скорее всего тут нужно создать отдельный контейнер для отображения
+    }
+    Прошу извинить за не выполненное первое задание, чекал в гугле, чтобы про анализировать как там, чтобы написать,
+    но там для Js подключают JQuery, поэтому я напишу со своих слов примерно...
+
+    пустой массив для хранения товаров на которые нажали добавить в корзину
+    
+    метод, который будет добавлять по клику в массив товар
+
+    метод, который будет выводить title и prise товаров считая стоимость.
+
+
+    (возможно нужно сделать 2 класса, 1 для отображения корзины в Html,
+         другой для добавления и отображения, но это не точно)
+
+         на этой ноте я пойду спать, сильно не ругайте)) zZzZzZz
+}*/
+
+
